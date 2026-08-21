5ba9b3c3157ef3af3b1cd1ae72c4187a12792550
Lucas-Cordoba <cssmcordoba@gmail.com>
2026-08-21 15:45:07 -0300

    'Se crearon rutas y el dashboard de crear proyecto'

--- uptask_frontend/src/router.tsx
+++ uptask_frontend/src/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "@/Layouts/AppLayout";
 import DashboardView from "@/views/DashboardView";
+import CreateProjectView from "./views/projects/CreateProjectView";
 
 export default function Router() {
 
@@ -10,6 +11,7 @@ export default function Router() {
             <Routes>
                 <Route element={<AppLayout/>}>
                     <Route path="/" element={<DashboardView/>} index/> {/*Le ponemos index porque es la pagina principal */}
+                    <Route path="/projects/create" element={<CreateProjectView/>} index/> {/*Le ponemos index porque es la pagina principal */}
                 </Route>
             </Routes>
         </BrowserRouter>
