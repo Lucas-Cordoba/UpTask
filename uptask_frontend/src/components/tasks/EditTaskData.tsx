8aac42e8e834f99e48f5252e92df26710bd2c993
Lucas-Cordoba <cssmcordoba@gmail.com>
2026-09-21 16:09:28 -0300

    'Se corrigieron mas errores'

--- uptask_frontend/src/components/tasks/EditTaskData.tsx
+++ uptask_frontend/src/components/tasks/EditTaskData.tsx
@@ -6,11 +6,11 @@ import EditTaskModal from "./EditTaskModal"
 export default function EditTaskData() {
 
     const params = useParams()
-    const projectId = params.projectId
+    const projectId = params.projectId!
 
     const location = useLocation()
     const queryParams = new URLSearchParams(location.search) //toma la cadena de consulta (query string) de la URL (por ejemplo, ?taskId=123&action=edit) y crea una instancia de objeto que facilita la lectura de sus valores.
-    const taskId = queryParams.get('editTask')
+    const taskId = queryParams.get('editTask')!
     
 
     const {data, isError} = useQuery({
